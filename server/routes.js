2813aade3553479ca65af19d919553c841b3ca19
Steven Chang <zminsc@seas.upenn.edu>
2024-10-22 14:51:07 -0400

    Scaffold backend

---
+++ server/routes.js
@@ -0,0 +1,27 @@
+const { Pool, types } = require('pg');
+const config = require('./config.json')
+
+// Override the default parsing for BIGINT (PostgreSQL type ID 20)
+types.setTypeParser(20, val => parseInt(val, 10)); //DO NOT DELETE THIS
+
+// Create PostgreSQL connection using database credentials provided in config.json
+// Do not edit. If the connection fails, make sure to check that config.json is filled out correctly
+const connection = new Pool({
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+connection.connect((err) => err && console.log(err));
+
+const helloWorld = async function(req, res) {
+    res.send('Hello, world!')
+}
+
+module.exports = {
+    helloWorld,
+}

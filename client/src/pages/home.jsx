9528973e11b6affbe027c964266f7ba4d6efccba
cynzhang26 <cynzhang@wharton.upenn.edu>
2024-12-13 15:24:39 -0500

    Fixed syntax error

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -43,7 +43,7 @@ export default function HomePage() {
             key={recipe.id}
             recipeItem={recipe}
           />
-        ))} : <div className="flex items-center justify-center h-screen"><Spinner color="warning"/></div>
+        ))}
       </div>
     </div>
   );
